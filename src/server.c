dd38159219e82b6d5dbf59e10260e47a5b3e806a
Youssef-ELb <81980094+Youssef-ELb@users.noreply.github.com>
2022-04-25 10:06:07 +0200

    update1

--- src/server.c
+++ src/server.c
@@ -64,24 +64,6 @@ int main(int argc, char *argv[])
             
             close(connfd);
         }
-        char hostbuffer[256];
-    char *IPbuffer;
-    struct hostent *host_entry;
-    int hostname;
-  
-    // To retrieve hostname
-    hostname = gethostname(hostbuffer, sizeof(hostbuffer));
-    checkHostName(hostname);
-  
-    // To retrieve host information
-    host_entry = gethostbyname(hostbuffer);
-    checkHostEntry(host_entry);
-  
-    // To convert an Internet network
-    // address into ASCII string
-    IPbuffer = inet_ntoa(*((struct in_addr*) host_entry->h_addr_list[0]));
-  
-    printf("Hostname: %s\n", hostbuffer);
-    printf("Host IP: %s", IPbuffer);
+       
     }
 }
