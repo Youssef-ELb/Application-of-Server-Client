0d00ce1d9a904fb8d55aaa06b1d3b49ae36ae399
mouhssineEL <mouhssine.el-idrissi@insa.ueuromed.org>
2022-05-17 13:28:47 +0200

    recupper de addresse ip

--- src/client.c
+++ src/client.c
@@ -1,13 +1,86 @@
- #include <sys/socket.h>
-#include <sys/types.h>
-#include <netinet/in.h>
-#include <netdb.h>
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+#include <net/if.h>
 #include <unistd.h>
+#include <linux/if.h>
 #include <errno.h>
+#include <ifaddrs.h>
+#include <netinet/in.h>
 #include <arpa/inet.h>
+
+char * get_ip_addr()
+{
+ 
+     //create an ifreq struct for passing data in and out of ioctl
+        struct ifreq my_struct;
+     	char * addr;
+     	
+        //declare and define the variable containing the name of the interface
+        char *interface_name="ens33";   //a very frequent interface name is "eth0";
+     
+        //the ifreq structure should initially contains the name of the interface to be queried. Which should be copied into the ifr_name field.
+        //Since this is a fixed length buffer, one should ensure that the name does not cause an overrun
+        size_t interface_name_len=strlen(interface_name);
+     
+        if(interface_name_len<sizeof(my_struct.ifr_name))
+        {
+            memcpy(my_struct.ifr_name,interface_name,interface_name_len);
+            my_struct.ifr_name[interface_name_len]=0;
+        }
+        else
+        {
+            perror("Copy name of interface to ifreq struct");
+            printf("The name you provided for the interface is too long...\n");
+        }
+     
+        //provide an open socket descriptor with the address family AF_INET
+        /* ***************************************************************
+         * All ioctl call needs a file descriptor to act on. In the case of SIOCGIFADDR this must refer to a socket file descriptor. This socket must be in the address family that you wish to obtain (AF_INET for IPv4)
+         * ***************************************************************
+         */
+     
+        int file_descriptor=socket(AF_INET, SOCK_DGRAM,0);
+     
+        if(file_descriptor==-1)
+        {
+            perror("Socket file descriptor");
+            printf("The construction of the socket file descriptor was unsuccessful.\n");
+            return 0;
+        }
+     
+        //invoke ioctl() because the socket file descriptor exists and also the struct 'ifreq' exists
+        int myioctl_call=ioctl(file_descriptor,SIOCGIFADDR,&my_struct);
+     
+        if (myioctl_call==-1)
+        {
+            perror("ioctl");
+            printf("Ooops, error when invoking ioctl() system call.\n");
+            close(file_descriptor);
+            return 0;
+        }
+     
+        close(file_descriptor);
+     
+        /* **********************************************************************
+         * If this completes without error , then the hardware address of the interface should have been returned in the  'my_struct.ifr_addr' which is types as struct sockaddr_in.
+         * ***********************************************************************/
+     
+      //extract the IP Address (IPv4) from the my_struct.ifr_addr which has the type 'ifreq'
+     
+        /* *** Cast the returned address to a struct 'sockaddr_in' *** */
+        struct sockaddr_in * ipaddress= (struct sockaddr_in *)&my_struct.ifr_addr;
+       /* *** Extract the 'sin_addr' field from the data type (struct) to obtain a struct 'in_addr' *** */
+	
+	addr = inet_ntoa(ipaddress->sin_addr);
+	
+
+return addr;
+}
+
+
 
 int main(int argc, char *argv[])
 {
@@ -67,9 +140,14 @@ int main(int argc, char *argv[])
         snprintf(sendBuff, sizeof(sendBuff), "%s\n", hostname);
         //Envoyer le contenu de sendBudd avec la socket
         write(sockfd, sendBuff, sizeof(sendBuff));
-        
+           //#############################################################################
+        //la recupperation de @ip
+           char * ip = get_ip_addr();
+          printf("\nvotre adresse ip est : %s", ip);
+          //#############################################################################
         //attiendre la connexion
-        close(sendBuff);
+        close(sockfd);
+      
     }
     
     return 0;
